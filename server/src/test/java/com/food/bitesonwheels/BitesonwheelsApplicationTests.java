380507dfdb7fe03266372ef571cb15950fd91768
prakash-1006 <prakash.27csb@licet.ac.in>
2026-08-31 10:50:20 +0530

    first commit

---
+++ server/src/test/java/com/food/bitesonwheels/BitesonwheelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.bitesonwheels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitesonwheelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
