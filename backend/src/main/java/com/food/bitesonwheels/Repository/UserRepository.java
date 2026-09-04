54bc90b69fea40af2d64e481a0148c1b98ee5c26
Dharsanrameshkumar <rdharsan322@gmail.com>
2026-09-04 12:29:54 +0530

    feat: implement JWT-based authentication system with user registration and login endpoints

--- backend/src/main/java/com/food/bitesonwheels/Repository/UserRepository.java
+++ backend/src/main/java/com/food/bitesonwheels/Repository/UserRepository.java
@@ -3,8 +3,12 @@ package com.food.bitesonwheels.Repository;
 import com.food.bitesonwheels.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.*;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existByEmail(String email);
+    boolean existByPhone(String phone);
     
 }
--- backend/src/main/java/com/food/bitesonwheels/Services/AuthService.java
+++ backend/src/main/java/com/food/bitesonwheels/Services/AuthService.java
@@ -1,1 +1,12 @@
 package com.food.bitesonwheels.Services;
+public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+    public User re
+}
